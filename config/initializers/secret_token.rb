d472c85e497782990a131d813e79d8d634f71f70
alamakot <dmichalekk@gmail.com>
2014-01-12 16:32:30 +0100

    ds
    asd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atencja::Application.config.secret_key_base = '4fec73678aca35b7876a5c832947032a8e4617036e19f03d828ce30e372f67ed6e897964dc51409109ee9f09374fc29a9d04b24c77bd7aae1e82585c1e0a22a8'
